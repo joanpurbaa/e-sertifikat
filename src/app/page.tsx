5ec46b675d1f0f9315a72b789208939c36664194
joanpurbaa <joanpurba562@gmail.com>
2025-03-08 20:22:52 +0700

    feat : add next auth session provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { signOut } from "next-auth/react";
+
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-red-500">hello world</h1>
-    </>
-  )
+	const { data: session } = useSession();
+
+	return (
+		<>
+			<h1 className="text-red-500">hello {session?.user?.name}</h1>
+			<form
+				action={async () => {
+					await signOut();
+				}}>
+				<button type="submit">Sign Out</button>
+			</form>
+		</>
+	);
 }
